0499e90510e8b30b04c0dffddae96fdc5a2851f3
Rishabh Mishra <42887318+rishabhhmishra@users.noreply.github.com>
2021-07-12 19:13:02 +0530

    Add files via upload

---
+++ Day15/app.js
@@ -0,0 +1,25 @@
+  
+"use strict";
+
+const e = React.createElement;
+
+class  Newclass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };  
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return "Hello World!";
+    }
+
+    return React.createElement(
+      "button",
+      { onClick: () => this.setState({ clicked: true }) },
+      "click me"
+    );
+  }
+}
+
+ReactDOM.render(React.createElement( Newclass), document.getElementById("root"));
