7ead51d4ae5ec1269827fdcd19eb0b7bed099354
Rishabh Mishra <42887318+rishabhhmishra@users.noreply.github.com>
2021-07-14 14:36:26 +0530

    Add files via upload

---
+++ react-tha/src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import Card from "./Components/MemeCard";
+
+function App() {
+	return (
+		<div className="App">
+			<Card />
+		</div>
+	);
+}
+
+export default App;
---
+++ react-tha/src/Components/MemeCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css";
+
+function Card() {
+	return (
+		<div className="card">
+			<img src="https://icon2.cleanpng.com/20180410/gfw/kisspng-michael-jordan-crying-jordan-air-jordan-t-shirt-michael-jordan-5acd3f753b42f6.0985847715234005652427.jpg" alt="meme" />
+			<h1>React Card</h1>
+			<h2>soo much backlogs..</h2>
+		</div>
+	);
+}
+export default Card;
